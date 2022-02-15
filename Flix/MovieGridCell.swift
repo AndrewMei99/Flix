bd6511823a874f487bb88c1795c6c8b9803375ac
Andrew Mei <andrew_mei@yahoo.com>
2022-02-15 11:41:38 -0800

    Part 2 Code
    Part 2 of Flix code completed

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Andrew Mei on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
